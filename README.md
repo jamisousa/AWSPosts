51f22f29495631ea86b7091ada541c7f16b8897a
0502j <jamiledsousa@gmail.com>
2023-02-28 11:23:52 -0300

    Update project
    Allowing post creation for signed in users

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 <img src="https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white">
 <img src="https://img.shields.io/badge/Amazon%20DynamoDB-4053D6?style=for-the-badge&logo=Amazon%20DynamoDB&logoColor=white">
 <img src="https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&logo=graphql&logoColor=white">
+    <img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white">
 </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import {API} from 'aws-amplify';
 import {listPosts} from '../graphql/queries';
+import Link from 'next/link';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -18,11 +19,16 @@ export default function Home() {
 
   return (
       <div>
-        <h1 className='text-pink-400 text-3xl font-bold'>My posts</h1>
+        <h1 className='text-pink-400 text-3xl font-bold tracking-wide mt-6 mb-2'>My posts</h1>
         <div className='py-2'></div>
         {
         posts.map((post, index)=>(
-          <p key={index}>{post.title}</p>
+          <Link key={index} href={`/posts/${post.id}`}>
+            <div className='cursor-pointer border=b border-gray-300 mt-8 pb4'>
+              <h2 className='text-xl font-semibold' key={index}>{post.title}</h2>
+              <p className="text-gray-500 mt-2">Author: {post.username}</p>
+            </div>
+          </Link>
         )
         )}
       </div>
